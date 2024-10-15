78aee375730a97e7f53b905505a887810fe59a72
iwarley <116743664+iwarley@users.noreply.github.com>
2024-10-15 11:49:52 -0400

    aula do dia 15

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,39 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+
 type ProdutoType = {
   id:number,
   nome:string,
-  preco:string,
   descricao:string,
+  preco:string,
   imagem:string
 }
 function App() {
-  const [nome, setNome] = useState("")
-  const [produtos,setProdutos] = useState([])
-  //useEffects(O que fazer, quando fazer) []=> Hora do carregamento
-useEffect(()=>{
-  setNome("iwarley quelis pique")
-  //buscar os dados do backend
-  fetch("https://one022a-marketplace1.onrender.com/produtos")
-  .then(resposta=>resposta.json())
-  .then(dados=>setProdutos(dados))
-  //colocar em uma variavel
-},[])
-{  
-  produtos.map(produto =>)
+  const [produtos, setProdutos] = useState<ProdutoType[]>([])
+  //useEffect(O QUe fazer, Quando Fazer)
+  useEffect(()=>{
+    fetch("https://one022a-marketplace1.onrender.com/produtos")
+    .then(resposta=>resposta.json())
+    .then(dados=>setProdutos(dados))
+  },[])
   return (
-    <>
-      <h1>{nome}</h1>
-      <div className='produtos-container'>
-        <div className='produto-item'>
-          <h1>Nome</h1>
-          <p>Imagem</p>
-          <p>Preço</p>
-          <p>Descrição</p>
-        </div>
-      </div> 
+    <>  
+      <div className="container-produtos">
+        {produtos.map(prod=>{
+          return(
+            <div key={prod.id} className="produto-item">
+              <h1>{prod.nome}</h1>
+              <div className='container-imagem'></div>
+              <img src={prod.imagem} alt="Imagem de celular" />
+              <p>{prod.preco}</p>
+              <p>{prod.descricao}</p>
+            </div>
+          )
+        })}
+      </div>
+      
     </>
   )
-}}
-
+}
 
 export default App
